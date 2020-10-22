f2cddad7530835c09fe09ae839702173739f4b22
Laurent Aubry <laurent.aubry@andre-legroupe.com>
2020-10-22 15:29:37 +0200

    Header fonctionnel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
 
-import Films from './pages/films'
+import Header from "./header/header";
+
+import Films from "./pages/films";
+import Musiques from "./pages/musiques";
 
 function App() {
   return (
     <div className="App">
       {/* Mise à jour du fichier pour test repo */}
       <h1>Bienvenue au GRETA</h1>
-      <Films />
+      <Router>
+        <Header />
+        <Route path="/" exact>
+          <Films />
+        </Route>
+        <Route path="/musiques" exact>
+          <Musiques />
+        </Route>
+        
+      </Router>
     </div>
   );
 }
